320d2aa2544076ffa26e61a425385a7c9b32859c
efrice <719431749@qq.com>
2024-04-20 23:08:07 +0800

    feat: favicon + icon

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
 import TheCount from '~/components/TheCount.vue'
 </script>
 
-<template>    
-  <p text-center mt-10>
-    <a rel="noreferrer" href="https://github.com/efrice/vue-start" target="_blank">
-      Vue Start
-    </a>
-  </p>
+<template>
+  <div text-center>
+    <div i-carbon-harbor  inline-block text-4xl mt-8 />
+    <p mt-2>
+      <a rel="noreferrer" href="https://github.com/efrice/vue-start" target="_blank">
+        Vue Start
+      </a>
+    </p>
+  </div>
   <TheCount :initial="10" />
 </template>
 
